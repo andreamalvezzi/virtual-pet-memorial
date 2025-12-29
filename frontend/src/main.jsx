18572b4fc12652b4297a0c2247613afac7229135
andreamalvezzi <andrea.mal67@gmail.com>
2025-12-29 13:16:41 +0100

    Fix HashRouter import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";   // 👈 QUESTA RIGA
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
