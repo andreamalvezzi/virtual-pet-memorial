a25c9ad305c33e3a22113f48441d8e3be9f405a7
andreamalvezzi <andrea.mal67@gmail.com>
2026-01-04 11:35:30 +0100

    fix: stabilize app with HelmetProvider and aligned deps

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";   // 👈 QUESTA RIGA
+import { HashRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./index.css";
-import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </HashRouter>
+    <HelmetProvider>
+      <HashRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </HashRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
